2d2ae8df48f6a82f5ddabc7596f415e0a5fae2fe
Slendi <slendi@socopon.com>
2023-10-12 22:02:19 +0300

    Nix: Fix flake.nix.
    Signed-off-by: Slendi <slendi@socopon.com>

--- flake.nix
+++ flake.nix
@@ -1,37 +1,39 @@
 {
-  description = "A CharaChorder configuration tool made in Qt6.";
-  nixConfig.bash-prompt = "\\[\\033[01;32m\\][nix-develop:\\w]$ \\[\\033[00m\\]";
+	description = "A CharaChorder configuration tool made in Qt6.";
+	nixConfig.bash-prompt = "\\[\\033[01;32m\\][nix-develop:\\w]$ \\[\\033[00m\\]";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-  };
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+	};
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-      {
-        devShells.${system}.default =
-          pkgs.mkShell
-            {
-              hardeningDisable = [ "all" ];
-              nativeBuildInputs = with pkgs; [
-                pkg-config
-                gcc
-                gnumake
-                cmake
-                ninja
-                clang-tools
-                valgrind
-              ];
-              buildInputs = with pkgs.buildPackages; [
-                qt6.qtbase
-                qt6.qtmultimedia
-              ];
-              
-              QT_PLUGIN_PATH="${pkgs.qt6.qtmultimedia}/lib/qt-6/plugins";
-            };
-      };
+	outputs = { self, nixpkgs }:
+		let
+			system = "x86_64-linux";
+			pkgs = nixpkgs.legacyPackages.${system};
+		in
+			{
+				devShells.${system}.default =
+					pkgs.mkShell
+						{
+							hardeningDisable = [ "all" ];
+							nativeBuildInputs = with pkgs; [
+								pkg-config
+								gcc
+								gnumake
+								cmake
+								ninja
+								clang-tools
+								valgrind
+							];
+							buildInputs = with pkgs.buildPackages; [
+								qt6.qtbase
+								qt6.qtmultimedia
+								qt6.qtserialport
+								qt6.qtserialbus
+							];
+
+							QT_PLUGIN_PATH="${pkgs.qt6.qtmultimedia}/lib/qt-6/plugins";
+						};
+			};
 
 }
